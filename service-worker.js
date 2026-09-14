0e0b460a044e7a6cd75247596306d53b72d36630
bernice-chua <chxian@gmail.com>
2026-09-14 18:56:52 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='car-depreciation-final-v16';
+const C='car-depreciation-family-v18';
 const A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./car-logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
